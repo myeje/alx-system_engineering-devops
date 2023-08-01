f81616332815884cf50a15f9c9cb21bc95c9a013
Joshua Emore <ehimareemore@gmail.com>
2023-08-01 14:20:31 -0700

    A regular expression that outputs [SENDER], [RECEIVER], [FLAGS]

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #A regular expression that outputs [SENDER], [RECEIVER], [FLAGS]
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
