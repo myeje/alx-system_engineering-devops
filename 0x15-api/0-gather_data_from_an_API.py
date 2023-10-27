fc3106497d180ecf05b76f943f0390ec4ba290f4
Joshua Emore <ehimareemore@gmail.com>
2023-10-28 00:21:17 +0100

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
 Python script that uses
-REST API for a given employee ID and returns info
+REST API for a given employeeId ID and returns info
 """
-import sys
 import requests
+import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     idUrl = "https://jsonplaceholder.typicode.com/"
     req = requests.get(idUrl + "todos", params={"userId": sys.argv[1]}).json()
-    employeeInfo = requests.get(idUrl + "users/{}".format(sys.argv[1])).json()
+    employeeId = requests.get(idUrl + "users/{}".format(sys.argv[1])).json()
 
     task = [pop.get("title") for pop in req if pop.get("completed") is True]
-    print("employeeInfo {} is done with tasks({}/{}):".format(
-		employeeInfo.get("name"), len(task), len(req)))
+    print("Employee {} is done with tasks({}/{}):".format(
+        employeeId.get("name"), len(task), len(req)))
     [print("\t {}".format(c)) for c in task]
