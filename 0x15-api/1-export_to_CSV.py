a18d0485121d3e50307fc2d45255c9d67b211f5d
Joshua Emore <ehimareemore@gmail.com>
2023-10-28 00:57:02 +0100

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     req = requests.get(idUrl + "todos", params={"userId": sys.argv[1]}).json()
     employeeId = requests.get(idUrl + "users/{}".format(sys.argv[1])).json()
     user = employeeId.get("user")
-
     task = [pop.get("title") for pop in req if pop.get("completed") is True]
+
     with open("{}.csv".format(sys.argv[1]), "w", newline="") as f:
-        for tsk in task:
+        for tsk in req:
             f.write('"{}","{}","{}","{}"\n'
                        .format(sys.argv[1], user, tsk.get('completed'),
                                tsk.get('title')))
