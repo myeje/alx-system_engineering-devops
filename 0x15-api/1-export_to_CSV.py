d6628f103fca1ed0ad13ac1e2e836687c7ff6581
Joshua Emore <ehimareemore@gmail.com>
2023-10-28 01:09:49 +0100

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     user = employeeId.get("user")
     task = [pop.get("title") for pop in req if pop.get("completed") is True]
 
-    with open("{}.csv".format(sys.argv[1]), "w", newline="") as f:
-        for tsk in req:
-            f.write('"{}","{}","{}","{}"\n'
-                    .format(sys.argv[1], user, tsk.get('completed'),
-                    tsk.get('title')))
+    with open("{}.csv".format(sys.argv[1]), "w", newline="") as file:
+        w = csv.writer(file, quoting=csv.QUOTE_ALL)
+        [w.writerow(
+            [sys.argv[1], user, tsk.get("completed"), tsk.get("title")]
+         ) for tsk in req]
