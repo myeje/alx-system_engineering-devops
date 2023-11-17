a049f7675830e9ac2edf4dde7c22584fdd66cdc3
Joshua Emore <ehimareemore@gmail.com>
2023-11-17 02:53:34 +0100

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Fixing web server that's not doing well
 
 file { 'fix-request':
-  ensure => 'file',
+  ensure  => 'file',
+  path    => '/etc/default/nginx',
   command => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
-  path    => '/etc/default/nginx'
 }
 
-# Restart Nginx
+# Restarting Nginx
 -> exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
